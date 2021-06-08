18e3c1b98178fb603f7be5e479dfd1d730d49ce5
Mikhail Kashirskiy <mkashirsky@gmail.com>
2021-06-08 19:17:31 +0300

    Update botCore.py

--- core/botCore.py
+++ core/botCore.py
@@ -35,7 +35,7 @@ cache_dir = 'logs/'.format(cwd)
 settings = {'public_key':'', 'private_key':'', 'host_ip':'127.0.0.1', 'host_port':5000, 'max_candles':500,'max_depth':50}
 
 class BotFront():
-    def __call__(self, settings, logs_dir, cache_dir):
+    def __init__(self, settings, logs_dir, cache_dir):
         # Initilization for the bot core managment object.
         logging.info('[BotCore] Initilizing the BotCore object.')
 
